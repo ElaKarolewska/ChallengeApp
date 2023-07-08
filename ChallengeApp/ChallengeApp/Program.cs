289a7318cfee66b18d91d7e3efc39f36fbb12be0
ElaKarolewska <e.karolewska00@gmail.com>
2023-07-08 22:26:57 +0200

    Zadanie dzień 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,28 @@
+﻿var name = "Ewa";
+var women = true;
+var age = 33;
+
+if (women)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+}
+else
+
+    if (!women && age < 18)
+{
+    Console.WriteLine("Niepełonoletni mężczyzna");
+
+}
+else  
+{ 
+}
+
+
+

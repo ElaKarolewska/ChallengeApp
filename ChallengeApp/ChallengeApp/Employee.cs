9c92faa49be42dd1252030fe41820926d0a90fe2
ElaKarolewska <e.karolewska00@gmail.com>
2023-07-24 23:01:28 +0200

    zadanie dzień 10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,26 +1,54 @@
 ﻿
+
 namespace ChallengeApp
 {
     public class Employee
     {
         private List<float> grades = new List<float>(); 
-        
         public Employee(string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
         }
-
-        public string Name { get; private set; }
-
-        public string Surname { get; private set; }
-
-        public void AddGrade(float grade) 
+         public string Name { get; private set; }
+         public string Surname { get; private set; }
+         public void AddGrade(float grade) 
+         {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else 
+            {
+                Console.WriteLine("invalid grade value");
+            }
+         }
+         public void AddGrade(string grade) 
+         {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else 
+            {
+                Console.WriteLine("String is not float");
+            }
+         }
+         public void AddGrade(long grade) 
+         {
+            float result = grade;
+            this.AddGrade(grade);
+         }
+        public void AddGrade(int grade) 
         {
-            this.grades.Add(grade);
-        
+            float result = grade;
+            this.AddGrade(grade);
         }
-
+        public void AddGrade(double grade) 
+        {
+            float result = (float) grade;
+            this.AddGrade(grade);
+        }
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
@@ -38,6 +66,5 @@ namespace ChallengeApp
             statistics.Average /= this.grades.Count;
             return statistics;
         }
-
     }
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿using ChallengeApp;
 
 var employee = new Employee("Ela", "Karolewska");
-employee.AddGrade(5);
+employee.AddGrade("5");
 employee.AddGrade(6);
 employee.AddGrade(4);
+employee.AddGrade(111245);
 var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Min: {statistics.Min}");

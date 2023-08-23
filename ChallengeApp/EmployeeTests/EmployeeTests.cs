f9500c074b5b9c257274851f4d4fa77331b9a990
ElaKarolewska <e.karolewska00@gmail.com>
2023-08-23 21:16:58 +0200

    Naprawione testy

---
+++ ChallengeApp/EmployeeTests/EmployeeTests.cs
@@ -0,0 +1,84 @@
+﻿using ChallengeApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTests
+{
+    internal class EmployeeTests
+    {
+        [Test]
+        public void WhenGetStatisticsAverage_ThenReceiveCorrectAverageLetter1()
+        {
+            var employee = new EmployeeInMemory("Ela", "Karolewska");
+            employee.AddGrade(5);
+            employee.AddGrade(6);
+            employee.AddGrade(4);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual('E', statistics.AverageLetter);
+        }
+        [Test]
+        public void WhenGetStatisticsAverage_ThenReciveCorrectAverageLetter2()
+        {
+            var employee = new EmployeeInMemory("Anna", "Wójcik");
+            employee.AddGrade(50);
+            employee.AddGrade(30);
+            employee.AddGrade(40);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual('C', statistics.AverageLetter);
+        }
+        [Test]
+        public void CheckIfAddGradeInLetterWorks()
+        {
+            var employee = new EmployeeInMemory("Marta", "Nowak");
+            employee.AddGrade('a');
+            employee.AddGrade('C');
+            employee.AddGrade(5);
+
+            var statistics = employee.GetStatistics();
+
+            Assert.AreEqual(55, statistics.Average);
+        }
+
+        [Test]
+        public void MinValueTest()
+        {
+            var employee = new EmployeeInMemory("Ela", "Karolewska");
+            employee.AddGrade(5);
+            employee.AddGrade(6);
+            employee.AddGrade(4);
+
+            Statistics statistics = employee.GetStatistics();
+
+            Assert.AreEqual(4, statistics.Min);
+
+        }
+
+        [Test]
+
+        public void AverageValueTest()
+        {
+            var employee = new EmployeeInMemory("Ela", "Karolewska");
+            employee.AddGrade(5);
+            employee.AddGrade(6);
+            employee.AddGrade(4);
+
+            Statistics statistics = employee.GetStatistics();
+
+            Assert.AreEqual(5, statistics.Average);
+        }
+
+
+        private EmployeeInMemory GetEmployee(string name, string surname)
+        {
+            return new EmployeeInMemory(name, surname);
+
+        }
+    }
+}
--- ChallengeApp/EmployeeTests/UnitTest1.cs
+++ ChallengeApp/EmployeeTests/UnitTest1.cs
@@ -2,6 +2,10 @@ namespace EmployeeTests
 {
     public class Tests
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
 
         [Test]
         public void Test1()
